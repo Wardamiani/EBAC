8a99705f5e842fe4dca2f97037e20a82b5a8b5c3
Wardamiani <wesley.damiani@icloud.com>
2023-05-22 17:25:54 -0300

    Gulp parte 2
    Finalização das tarefas, com gulp server rodando e com atualização instantâneas a partir da pasta SRC.

--- Aula 21/projeto/gulpfile.js
+++ Aula 21/projeto/gulpfile.js
@@ -1,46 +1,57 @@
+const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
-// const stripJs = require('gulp-strip-comments')
-// const stripCss = require('gulp-strip-css-comments')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync')
+const browser = require('browser-sync').create()
+const reload = browserSync.reload
 
-function tarefasCSS(cb) {
+function tarefasCSS(callback) {
 
-    return gulp.src([
+    gulp.src([
         './node_modules/bootstrap/dist/css/bootstrap.css',
         './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
         './vendor/owl/css/owl.css',
         './vendor/jquery-ui/jquery-ui.css',
         './src/css/style.css'
-    ])
-        // .pipe(stripCss())                   // remove comentários
-        .pipe(concat('styles.css'))         // mescla arquivos
-        .pipe(cssmin())                     // minifica css
-        .pipe(rename({ suffix: '.min'}))    // styles.min.css
-        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
-
+    ])        
+        .pipe(concat('styles.css'))         
+        .pipe(cssmin())                     
+        .pipe(rename({ suffix: '.min'}))    
+        .pipe(gulp.dest('./dist/css'))   
+        
+    return callback()
 }
 
-function tarefasJS(){
+function tarefasJS(callback){
 
-    return gulp.src([
+    
+
+    gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
             './vendor/jquery-mask/jquery.mask.js',
-            './vendor/jquery-ui/jquery-ui.js',
+            // './vendor/jquery-ui/jquery-ui.js',
             './src/js/custom.js'
         ])
 
-        .pipe(concat('scripts.js'))         // mescla arquivos
-        .pipe(uglify())                     // minifica js
-        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
-        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))     
+        .pipe(concat('scripts.js'))         
+        .pipe(uglify())                    
+        .pipe(rename({ suffix: '.min'}))   
+        .pipe(gulp.dest('./dist/js'))       
+    
+    return callback()    
 }
-
 
 function tarefasImagem(){
     
@@ -59,6 +70,33 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+function tarefasHTML(callback){
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+    
+    return callback()
+}
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+})
+
+function end(cb){
+    console.log("tarefas concluídas")
+    return cb()
+}
+
+const process = series(tarefasHTML, tarefasJS, tarefasCSS, end)
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+exports.default = process
